756e70311416834ff6b58d76b96519d9b2d9fa4e
Roger Chappel <miscanalysis@gmail.com>
2026-05-04 06:37:42 +1000

    Expose public TypeScript API

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { estimateModelUsd, roundUsd } from "./cost.js";
+export { formatJson, formatMarkdown } from "./format.js";
+export { inspectWorkspace, flattenModels } from "./inspect.js";
+export { loadWorkspace, validateWorkspace } from "./parser.js";
+export { ConfigError, ModelgateError } from "./errors.js";
+export type * from "./types.js";
